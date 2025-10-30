b5c2b96e3b2b5b06d0331f0735e07f87527c3d2d
Astrid <acevedovargasastridjuliana@gmail.com>
2025-10-30 10:31:16 -0500

    Subio Archivo Asp.Net

---
+++ AppPruebaGrupo1Git/AppPruebaGrupo1Git/Datos/ClConexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AppPruebaGrupo1Git.Datos
+{
+    public class ClConexion
+    {
+        SqlConnection oConex;
+
+        public ClConexion()
+        {
+            oConex = new SqlConnection("");
+        }
+        public SqlConnection MtAbrirConexiom()
+        {
+            oConex.Open();
+            return oConex;
+        }
+        public void MtCerrarConexion()
+        {
+            oConex.Close();
+        }
+    }
+}
