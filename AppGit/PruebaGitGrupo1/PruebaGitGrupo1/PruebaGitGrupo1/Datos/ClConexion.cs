6e11a59a8a6194f9a026c7a56d35181642a449af
Astrid <acevedovargasastridjuliana@gmail.com>
2025-10-30 10:00:49 -0500

    Archivo Subido

--- AppGit/PruebaGitGrupo1/PruebaGitGrupo1/PruebaGitGrupo1/Datos/ClConexion.cs
+++ AppGit/PruebaGitGrupo1/PruebaGitGrupo1/PruebaGitGrupo1/Datos/ClConexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,22 @@ namespace PruebaGitGrupo1.Datos
 {
     public class ClConexion
     {
+        SqlConnection oConex;
+
+        public ClConexion()
+        {
+            oConex = new SqlConnection("Data Source=.;Initial Catalog=Grupo1;Integrated Security=True;");
+        }
+
+        public SqlConnection MtAbrirConexion()
+        {
+            oConex.Open();
+            return oConex;
+        }
+
+        public void MtCerrarConexion()
+        {
+            oConex.Close();
+        }
     }
 }
